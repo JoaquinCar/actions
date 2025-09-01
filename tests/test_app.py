8e4ced41a4a374ce8bc35a69ddba077dd97139e5
JoaquinCar <joaquincarmona758@gmail.com>
2025-08-31 18:40:59 -0600

    workflow 2- testss

--- tests/test_app.py
+++
@@ -1,5 +0,0 @@
-from app import suma
-
-def test_suma():
-    assert suma(2, 3) == 5
-    assert suma(-1, 1) == 0
---
+++ tests/test_core.py
@@ -0,0 +1,4 @@
+from app.core import suma
+
+def test_suma():
+    assert suma(2, 3) == 5
